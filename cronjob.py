3adfbd4fea5f643173127f57deb5dd4c752bf0f1
boa50 <bruno.albuquerque50@gmail.com>
2021-10-10 17:19:32 -0300

    Changes to run correctly on heroku

--- cronjob.py
+++ cronjob.py
@@ -3,6 +3,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from main import process_notifications
 
 scheduler = BlockingScheduler()
-scheduler.add_job(process_notifications, 'cron', second='*/30')
+scheduler.add_job(process_notifications, 'cron', hour='0', minute='30', second='0', jitter=120)
 
 scheduler.start()
